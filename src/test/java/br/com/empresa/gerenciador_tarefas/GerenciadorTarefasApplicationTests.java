66366b60920568f5317d9cc7a6037fecad29a8a6
Rodrigo <rodleidenschaft@gmail.com>
2025-09-18 23:33:57 -0300

    feat(ci): adiciona workflow inicial para build e teste com h2

--- src/test/java/br/com/empresa/gerenciador_tarefas/GerenciadorTarefasApplicationTests.java
+++ src/test/java/br/com/empresa/gerenciador_tarefas/GerenciadorTarefasApplicationTests.java
@@ -3,7 +3,7 @@ package br.com.empresa.gerenciador_tarefas;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = GerenciadorTarefasApplication.class)
 class GerenciadorTarefasApplicationTests {
 
 	@Test
